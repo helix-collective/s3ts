d065e598d36658b88afbf3d8acc9914c672f8ef6
Tim Docker <tim@dockerz.net>
2015-11-20 12:46:47 +1100

    Script to build standalone zip file
    Reviewers: nikhil
    
    Subscribers: #helix_devs
    
    Differential Revision: http://phab.helixta.com.au/D2135

--- README.md
+++ README.md
@@ -1,17 +1,36 @@
-# An S3 TreeStore (S3TS)
+# Overview
 
-This is a python library and command line tool to efficiently manage the storage of file trees within Amazon S3.
+This is a python library and command line tool to efficiently manage
+the storage of file trees within Amazon S3.
 
-## Running the tests
+# Setup the environment
 
 ```
 virtualenv python-venv
 ./python-venv/bin/pip install -r requirements.txt
+```
 
+# Running the tests
+
+```
 export AWS_ACCESS_KEY_ID=...
 export AWS_SECRET_ACCESS_KEY=...
 export S3TS_BUCKET=... an empty s3 bucket ...
 S3TS_LOCALCACHE=/tmp
 
 PYTHONPATH=./src ./python-venv/bin/python test/test_treestore.py
+```
+
+# Build a standalone zip file
+
+This builds a zip file than includes s3ts and it's dependencies.
+
+```
+./python-venv/bin/python tools/build-standalone-zip.py
+```
+
+It can be run directly from the command line:
+
+```
+python dist/s3ts.zip --help
 ```
---
+++ tools/build-standalone-zip.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import os, subprocess, zipfile
+
+BASE_DIR = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '..' ) )
+
+def newZipFile( zipPath ):
+    if not os.path.exists( os.path.dirname( zipPath ) ):
+        os.makedirs( os.path.dirname( zipPath ) )
+    return zipfile.ZipFile( zipPath, 'w' )
+
+def addFilesToZip( zf, dir, matcher=None, arcname=None ):
+    for root, dirnames, filenames in os.walk(dir):
+        for f in filenames:
+            path = os.path.join(root, f)
+
+            if not matcher or matcher(path):
+                if arcname:
+                    arcname1 = os.path.join( arcname, os.path.relpath( path, dir) )
+                else:
+                    arcname1 = os.path.relpath( path, dir)
+                zf.write( path, arcname=arcname1 )
+
+                
+# Build the s3ts python zip file
+# (which is used for uploading packages to S3)
+s3tsPythonZip = os.path.join( BASE_DIR, 'dist/s3ts.zip' )
+zf = newZipFile( s3tsPythonZip )
+addFilesToZip( zf, os.path.join( BASE_DIR, 'src' ), lambda f:f.endswith('.py') )
+addFilesToZip( zf, os.path.join( BASE_DIR, 'python-venv/lib/python2.7/site-packages/boto' ), lambda f:f.endswith('.py'), 'boto' )
+addFilesToZip( zf, os.path.join( BASE_DIR, 'python-venv/lib/python2.7/site-packages/requests' ), lambda f:f.endswith('.py'), 'requests' )
+zf.close()
